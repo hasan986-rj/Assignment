7f9ca244c63fe7effa5bf41d2112c734b0b819c0
M. Hasan <138823344+hasan986-rj@users.noreply.github.com>
2023-07-09 09:49:33 +0500

    Add files via upload

---
+++ ex20.ts
@@ -0,0 +1,6 @@
+/* 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages,
+ or anything else you’d like. Write a program that creates a list containing these items.*/
+let something :string[]=["Nanga Purbut","Kashmir","Ravi River","Turkey","TypeScript","Faisal Masjid",""]
+for (let i=0; i<something.length; i++ ){
+console.log(something[i]);
+}
